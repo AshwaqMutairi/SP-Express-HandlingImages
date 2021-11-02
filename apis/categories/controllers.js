63bd8efc7b50f4fd3889cbb7318551137a4792fe
ash m <ashkhmu@gmail.com>
2021-11-02 11:37:37 +0300

    testing

--- apis/categories/controllers.js
+++ apis/categories/controllers.js
@@ -3,7 +3,7 @@ const Category = require("../../db/models/Category");
 //get catrgory list
 exports.getCategories = async (req, res) => {
   try {
-    const categories = await Category.find();
+    const categories = await Category.find().populate("products");
     return res.json(categories);
   } catch (error) {
     return res.status(500).json({ message: error.message });
@@ -22,14 +22,15 @@ exports.categoryCreate = async (req, res) => {
 
 exports.productCreate = async (req, res) => {
   try {
+    const categoryId = req.params.categoryId;
+    req.body = { ...req.body, category: categoryId };
+    const newProduct = await Product.create(req.body);
+
     await Category.findOneAndUpdate(
       { _id: req.params.categoryId },
       { $push: { products: newProduct._id } }
     );
 
-    const categoryId = req.params.categoryId;
-    req.body = { ...req.body, category: categoryId };
-    const newProduct = await Product.create(req.body);
     return res.status(201).json(newProduct);
   } catch (error) {
     return res.status(500).json({ message: error.message });
