90fec02bfa354a3a1eef9189a9aa87871a4d5864
ash m <ashkhmu@gmail.com>
2021-11-03 10:45:13 +0300

    update product modified

--- apis/categories/controllers.js
+++ apis/categories/controllers.js
@@ -25,14 +25,20 @@ exports.categoryCreate = async (req, res) => {
 exports.productUpdate = async (req, res, next) => {
   try {
     if (req.file) {
-      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+      // req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+      req.body.image = `${req.protocol}://${req.get("host")}/media/${
+        req.file.filename
+      }`;
     }
-    const product = await Product.findByIdAndUpdate(
-      req.product,
-      req.body,
-      { new: true, runValidators: true } // returns the updated product
-    );
-    return res.status(200).json(product);
+    await req.product.update(req.body);
+    // const product = await Product.findByIdAndUpdate(
+    //   req.product,
+    //   req.body,
+    //   { new: true, runValidators: true } // returns the updated product
+    // );
+    // return res.status(200).json(product);
+    const product = await Product.findById(req.product._id);
+    res.status(200).json(product);
   } catch (error) {
     next(error);
   }
